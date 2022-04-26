6d742d29f61c0165eb8baadc43a45a87743c6730
Petrovich <popivbod12@gmail.com>
2022-04-26 10:28:54 +0300

    testing new uikernel version

--- src/index.js
+++ src/index.js
@@ -1,47 +1,16 @@
+/*
+ * Copyright (с) 2015-present, SoftIndex LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import UIKernel from "uikernel";
+import ReactDOM from 'react-dom';
 import 'uikernel/dist/themes/base/uikernel.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import './main.css';
+import MainComponent from './Components/MainComponent.js';
 
-const model = new UIKernel.Models.Grid.Collection({
-    data: [
-        [1, {
-            name: 'Pace',
-            surname: 'White',
-            age: 20
-        }],
-        [2, {
-            name: 'Evangeline',
-            surname: 'Terrell',
-            age: 72
-        }],
-        [3, {
-            name: 'Roach',
-            surname: 'Potts',
-            age: 14
-        }]
-    ]
-});
-
-const columns = {
-    name: {
-        name: 'First Name',
-        render: ['name', record => record.name]
-    },
-    surname: {
-        name: 'Last Name',
-        render: ['surname', record => record.surname]
-    },
-    age: {
-        name: 'Age',
-        render: ['age', record => record.age]
-    }
-};
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <UIKernel.Grid cols={columns} model={model}/>
-  </React.StrictMode>
-);
-
+ReactDOM.render(<MainComponent/>, document.getElementById(('root')));
---
+++ src/validator.js
@@ -0,0 +1,15 @@
+/*
+ * Copyright (с) 2015-present, SoftIndex LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import UIKernel from 'uikernel'
+
+const validator = UIKernel.createValidator()
+  .field('name', UIKernel.Validators.regExp.notNull(/^\w{2,30}$/, 'Invalid first name.'))
+  .field('age', UIKernel.Validators.number.notNull(15, 90, 'Age must be between 15 and 90'));
+
+export default validator
